2686ad236172d01a12d40810845957c4b38e4627
shouko <imshouko@gmail.com>
2023-08-27 00:45:26 +0900

    Factor out site specific handlers

--- app.js
+++ app.js
@@ -8,35 +8,81 @@ const addr = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 const upstream = process.env.UPSTREAM || '';
+const allowedElUpstreams = (process.env.ALLOWED_EL_UPSTREAMS || '').split(',');
+
 const playlistMime = 'application/x-mpegURL';
 const segmentMime = 'video/mp2t';
+const keyRgx = /^[0-9a-f]{32}$/;
 
-const initSegCache = new Map();
-const extractExt = (s) => s.substr(s.lastIndexOf('.'));
+const extractExt = (s) => s.substr(s.lastIndexOf('.')).toLowerCase();
+
+const stgcrHandler = async (url, key, res) => {
+  if (!key.match(keyRgx)) throw new Error();
+
+  const ext = extractExt(url.pathname);
+  if (ext == '.m3u8') {
+    const resp = await fetch(`${upstream}${url.pathname}`);
+    res.statusCode = 200;
+    res.setHeader('content-type', playlistMime);
+    const filtered = resp.split("\n").filter((l) => !l.startsWith('#EXT-X-KEY') && !l.startsWith('#EXT-X-MAP:URI'));
+    const transformed = filtered.map((l) => (l.startsWith('#') || !l.length) ? l : `${l}${l.endsWith('.m4s') ? '.ts' : ''}?key=${key}`).join("\n");
+    res.end(transformed);
+  } else if (ext == '.ts') {
+    const decrypted = await getSegment(`${upstream}${url.pathname.substr(0, url.pathname.lastIndexOf('.'))}`, key);
+    res.statusCode = 200;
+    res.setHeader('cache-control', 'public, max-age=86400');
+    res.setHeader('content-type', segmentMime);
+    res.end(decrypted);
+  }
+
+  throw new Error();
+};
+
+
+const elHandler = async (url, akey, vkey, res) => {
+  const burl = new URL(url.searchParams.get('url'));
+  if (!allowedElUpstreams.includes(burl.host)) throw new Error();
+
+  const bext = extractExt(burl.pathname);
+  if (bext == '.mpd') {
+    // generate m3u8 playlist base on mpd content
+    res.statusCode = 200;
+    res.setHeader('content-type', playlistMime);
+  }
+
+  const [
+    ainiturl,
+    abodyurl,
+    viniturl,
+    vbodyurl,
+  ] = ['ainit', 'apath', 'vinit', 'vpath'].map((qkey) => {
+    const u = new URL(url.searchParams.get(qkey), burl);
+    if (u.hostname !== burl.hostname) {
+      throw new Error(`Hostname ${u.hostname} for key ${qkey} invalid`);
+    }
+    const ext = extractExt(u.pathname);
+    if (['.mp4', '.m4s'].indexOf(ext) == -1) {
+      throw new Error(`Extension ${ext} for key ${qkey} invalid`);
+    }
+    return u;
+  })
+};
+
 
 const server = http.createServer(async (req, res) => {
   try {
     const url = new URL(req.url, `http://${req.headers.host}`);
     const key = url.searchParams.get('key') || "";
-
-    if (!key.match(/^[0-9a-f]{32}$/)) throw new Error();
-
+    const akey = url.searchParams.get('akey') || key;
+    const vkey = url.searchParams.get('vkey') || key;
     res.setHeader('access-control-allow-origin', '*');
-    const ext = extractExt(url.pathname);
-    if (ext == '.m3u8') {
-      const resp = await fetch(`${upstream}${url.pathname}`);
-      res.statusCode = 200;
-      res.setHeader('content-type', playlistMime);
-      const filtered = resp.split("\n").filter((l) => !l.startsWith('#EXT-X-KEY') && !l.startsWith('#EXT-X-MAP:URI'));
-      const transformed = filtered.map((l) => (l.startsWith('#') || !l.length) ? l : `${l}${l.endsWith('.m4s') ? '.ts' : ''}?key=${key}`).join("\n");
-      res.end(transformed);
-    } else if (ext == '.ts') {
-      const decrypted = await getSegment(`${upstream}${url.pathname.substr(0, url.pathname.lastIndexOf('.'))}`, key);
-      res.statusCode = 200;
-      res.setHeader('cache-control', 'public, max-age=86400');
-      res.setHeader('content-type', segmentMime);
-      res.end(decrypted);
+
+    if (url.pathname == '/el') {
+      await elHandler(url, akey, vkey, res);
+      return;
     }
+
+    await stgcrHandler(key, url, res);
   } catch(e) {
     console.error(e);
     res.statusCode = 404;
--- segment.js
+++ segment.js
@@ -4,23 +4,15 @@ const fetch = require('./fetch');
 
 const extractPath = (s) => s.substr(0, s.lastIndexOf('/'));
 
-const getSegment = (url, key) => new Promise(async (resolve, reject) => {
-  const [ segInit, segBody ] = await Promise.all([
-    fetch(`${extractPath(url)}/init.mp4`),
-    fetch(url),
-  ]);
-
-  const ffArgs = [
+const createFfmpeg = (inputArgs, cb) => {
+  const output = [];
+  const ff = spawn(ffmpegPath, [
     '-loglevel', 'error',
-    '-f', 'mp4',
-    '-decryption_key', key,
-    '-i', 'pipe:0', '-c', 'copy',
+    ...inputArgs,
+    '-c', 'copy',
     '-copyts', '-f', 'mpegts',
     'pipe:1'
-  ];
-
-  const output = [];
-  const ff = spawn(ffmpegPath, ffArgs);
+  ]);
 
   ff.stdout.on('data', (data) => {
     output.push(data);
@@ -32,9 +24,29 @@ const getSegment = (url, key) => new Promise(async (resolve, reject) => {
   ff.on('close', (code) => {
     if (code !== 0) {
       console.error(`FFmpeg exited with ${code}`);
-      return reject(code);
+      return cb(code, null);
     }
-    return resolve(Buffer.concat(output));
+    return cb(null, Buffer.concat(output));
+  });
+
+  return ff;
+};
+
+const getSegment = (url, key, initUrl) => new Promise(async (resolve, reject) => {
+  const [ segInit, segBody ] = await Promise.all([
+    fetch(initUrl || `${extractPath(url)}/init.mp4`),
+    fetch(url),
+  ]);
+
+  const inputArgs = [
+    '-f', 'mp4',
+    '-decryption_key', key,
+    '-i', 'pipe:0',
+  ];
+
+  const ff = createFfmpeg(inputArgs, (err, data) => {
+    if (err) return reject(err);
+    return resolve(data);
   });
 
   ff.stdin.write(segInit);
@@ -42,6 +54,31 @@ const getSegment = (url, key) => new Promise(async (resolve, reject) => {
   ff.stdin.end();
 });
 
+const getCombinedSegment = async (ainiturl, abodyurl, akey, viniturl, vbodyurl, vkey) => {
+  const [
+    aseg,
+    vseg,
+  ] = await Promise.all([
+    getSegment(abodyurl, akey, ainiturl),
+    getSegment(vbodyurl, vkey, viniturl),
+  ]);
+
+  const inputArgs = [
+    '-f', 'mpegts',
+    '-i', 'pipe:0',
+  ];
+
+  const ff = createFfmpeg(inputArgs, (err, data) => {
+    if (err) return reject(err);
+    return resolve(data);
+  });
+
+  ff.stdin.write(aseg);
+  ff.stdin.write(vseg);
+  ff.stdin.end();
+}
+
 module.exports = {
-  getSegment
+  getSegment,
+  getCombinedSegment,
 };
